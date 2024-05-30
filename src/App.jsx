db21229ee95ec0b6402185e445585ffe7ac4b5c2
Muhammad Muzammal <muzamal503@gmail.com>
2024-05-30 14:56:46 +0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Notes from "./pages/Notes";
 import CreateNote from "./pages/CreateNote";
 import EditNote from "./pages/EditNote";
@@ -7,26 +7,30 @@ import EditNote from "./pages/EditNote";
 import { useEffect, useState } from "react";
 
 const App = () => {
-  const [notes, setNotes] = useState(JSON.parse(localStorage.getItem('notes')) || []);
+  const [notes, setNotes] = useState(
+    JSON.parse(localStorage.getItem("notes")) || []
+  );
   // console.log(notes);
-  
-  useEffect(() => {
-    localStorage.setItem('notes', JSON.stringify(notes))
 
-  }, [notes])
+  useEffect(() => {
+    localStorage.setItem("notes", JSON.stringify(notes));
+  }, [notes]);
 
   return (
     <main id="app">
-      <BrowserRouter>
+      <Router>
         <Routes>
           <Route path="/" element={<Notes notes={notes} />} />
           <Route
             path="/create-note"
             element={<CreateNote setNotes={setNotes} />}
           />
-          <Route path="/edit-note/:id" element={<EditNote notes={notes} setNotes={setNotes} />} />
+          <Route
+            path="/edit-note/:id"
+            element={<EditNote notes={notes} setNotes={setNotes} />}
+          />
         </Routes>
-      </BrowserRouter>
+      </Router>
     </main>
   );
 };
